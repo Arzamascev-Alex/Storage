5586e9ab47fb2a48e0a99fb326aad5d999d3b40f
Aleksey <arz.alexey@gmail.com>
2026-09-13 00:24:20 +0400

    Блокировки.
    Добылены блокировки: чтение (Get) и запись (Set, Delete).

--- Storage.Core/SimpleStore.cs
+++ Storage.Core/SimpleStore.cs
@@ -1,28 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 
 namespace Storage.Core
 {
-    public class SimpleStore
+    public class SimpleStore : IDisposable
     {
         private readonly Dictionary<string, byte[]> _storage = new();
+        private readonly ReaderWriterLockSlim _lock = new();
+
+        private long _setCount;
+        private long _getCount;
+        private long _deleteCount;
 
         public void Set(string key, byte[] value)
         {
-            _storage[key] = value;
+            _lock.EnterWriteLock();
+
+            try 
+            {
+                _storage[key] = value;
+
+                Interlocked.Increment(ref _setCount);
+
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+            
         }
 
         public byte[]? Get(string key)
         {
-            return _storage.TryGetValue(key, out byte[]? value)
-                ? value 
-                : null;
+            _lock.EnterReadLock();
+
+            try
+            {
+                _storage.TryGetValue(key, out byte[]? value);
+
+                Interlocked.Increment(ref _getCount);
+
+                return value;
+            }
+            finally
+            {
+                _lock.ExitWriteLock ();
+            }
+            
         }
 
         public void Delete(string key)
         {
-            _storage.Remove(key);
+            _lock.EnterWriteLock();
+
+            try
+            {
+                _storage.Remove(key);
+
+                Interlocked.Increment(ref _deleteCount);
+            }
+            finally
+            {
+                _lock.ExitWriteLock ();
+            }
+
         }
+
+        public (long setCount, long getCount, long deleteCount) GetStatistics()
+        {
+            return
+                (
+                    Interlocked.Read(ref _setCount),
+                    Interlocked.Read(ref _setCount),
+                    Interlocked.Read(ref _deleteCount)
+                );
+        }
+
+        public void Dispose()
+        {
+            _lock.Dispose();
+        }
+
     }
 }
